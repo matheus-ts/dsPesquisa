1727d55185e39c5fb2504ccec74a52ab23f6776a
MatheusCandido <matheus.csilva@bandtec.com.br>
2020-09-15 20:39:24 -0300

    Method/GET records

--- Backend/src/main/java/com/dspesquisa/controllers/RecordsController.java
+++ Backend/src/main/java/com/dspesquisa/controllers/RecordsController.java
@@ -6,9 +6,13 @@ import com.dspesquisa.dto.RecordInsertDTO;
 import com.dspesquisa.services.GamesService;
 import com.dspesquisa.services.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -20,8 +24,35 @@ public class RecordsController {
 
     @PostMapping
     public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto){
-        RecordDTO nDto = service.insert(dto);
+        RecordDTO nDto = new RecordDTO();
+        nDto = service.insert(dto);
         return ResponseEntity.ok().body(nDto);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<RecordDTO>> findAll(
+            @RequestParam(value= "min",defaultValue = "") String min,
+            @RequestParam(value="max", defaultValue = "") String max,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "DESC") String direction){
+
+        Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+
+        Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+
+        if(linesPerPage == 0 ){
+            linesPerPage = Integer.MAX_VALUE;
+        }
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction),orderBy);
+
+
+        Page<RecordDTO> list = service.findByMoments( minDate,maxDate,pageRequest);
+        return ResponseEntity.ok().body(list);
+    }
+
+
 
 }
--- Backend/src/main/java/com/dspesquisa/dto/RecordDTO.java
+++ Backend/src/main/java/com/dspesquisa/dto/RecordDTO.java
@@ -29,4 +29,64 @@ public class RecordDTO implements Serializable {
         gamePlatform = entity.getGame().getPlataform();
         genreName = entity.getGame().getGenre().getName();
     }
+
+    public static long getSerialvLersionUID() {
+        return serialvLersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGameTitle() {
+        return gameTitle;
+    }
+
+    public void setGameTitle(String gameTitle) {
+        this.gameTitle = gameTitle;
+    }
+
+    public Plataform getGamePlatform() {
+        return gamePlatform;
+    }
+
+    public void setGamePlatform(Plataform gamePlatform) {
+        this.gamePlatform = gamePlatform;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
+
+    public void setGenreName(String genreName) {
+        this.genreName = genreName;
+    }
 }
