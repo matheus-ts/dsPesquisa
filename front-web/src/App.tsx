d41606a214547670980eba94048752e1402c5403
MatheusCandido <matheus.csilva@bandtec.com.br>
2020-09-16 22:02:17 -0300

    adcionando component Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
-    <h2>Hello World</h2>
+    <div>
+      <Header />
+    </div>
   );
 }
 
