13c79a9afc38ffa14aba96c7d8ca1ec115fdac31
MatheusCandido <matheus.csilva@bandtec.com.br>
2020-09-16 22:21:53 -0300

    Criando page Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
